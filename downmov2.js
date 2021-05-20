e2b2ac102d21d9ee67c2a068aa8d11f80a0d55a7
gentaengine <84512694+gentaengine@users.noreply.github.com>
2021-05-20 21:28:53 +0800

    Create downmov2.js

---
+++ downmov2.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+  '//intendedeasiestlost.com/pysu8km2c0?key=30889f4bcf927a931b6648878347f2bb'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
